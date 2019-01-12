354ddd4583ca3647e73eab08167ad07cfcf92943
bangofett <ban.ngo01@gmail.com>
2019-01-13 04:22:39 +0900

    ui updates
    cleaned up UI and made some changes to the menu and added titles to charts

--- public/js/charts.js
+++ public/js/charts.js
@@ -9,8 +9,10 @@ function showBar(){
   // specify chart configuration item and data
   option = {
       title: {
-          text: 'Fruit Count'
-      },
+            text: 'Bar Chart',
+            subtext: '',
+            left: 'center'
+        },
       tooltip: {},
       legend: {
           data:['Total']
@@ -49,7 +51,9 @@ function showLine(){
     // specify chart configuration item and data
     var option = {
         title: {
-            text: 'The amount of people in the mall everyday'
+            text: 'Line Chart',
+            subtext: 'Number of People Through a Mall Everyday',
+            left: 'center'
         },
         tooltip: {},
         legend: {
@@ -90,9 +94,9 @@ function showPie(){
     // specify chart configuration item and data
     option = {
         title: {
-            text: 'Customized Pie',
-            left: 'center',
-            top: 20
+            text: 'Pie Chart',
+            subtext: 'Pie Survey',
+            left: 'center'
         },
     
         tooltip : {
@@ -110,29 +114,29 @@ function showPie(){
         },
         series : [
             {
-                name:'source of visit',
+                name:'',
                 type:'pie',
                 radius : '55%',
                 center: ['50%', '50%'],
                 data:[
-                    {value:335, name:'visit directly'},
-                    {value:310, name:'Email marketing'},
-                    {value:274, name:'Alliance advertising'},
-                    {value:235, name:'video ad'},
-                    {value:400, name:'search engine'}
+                    {value:335, name:'Apple'},
+                    {value:310, name:'Pumpkin'},
+                    {value:274, name:'Custard'},
+                    {value:235, name:'Meat Pie'},
+                    {value:400, name:'Fruit'}
                 ].sort(function (a, b) { return a.value - b.value; }),
                 roseType: 'radius',
                 label: {
                     normal: {
                         textStyle: {
-                            color: 'rgba(255, 255, 255, 0.3)'
+                            color: 'rgba(0, 0, 0)'
                         }
                     }
                 },
                 labelLine: {
                     normal: {
                         lineStyle: {
-                            color: 'rgba(255, 255, 255, 0.3)'
+                            color: 'rgba(0, 0, 0)'
                         },
                         smooth: 0.2,
                         length: 10,
@@ -143,7 +147,7 @@ function showPie(){
                     normal: {
                         color: '#c23531',
                         shadowBlur: 200,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        shadowColor: 'rgba(0, 0, 0)'
                     }
                 },
     
@@ -178,6 +182,13 @@ function showScatter(){
   
     // specify chart configuration item and data
     option = {
+
+        title: {
+            text: 'Scatter Chart',
+            subtext: '',
+            left: 'center'
+        },
+
         xAxis: {},
         yAxis: {},
         series: [{
@@ -249,7 +260,7 @@ function showMap(){
             max: 2500,
             left: 'left',
             top: 'bottom',
-            text: ['高','低'],           // 文本，默认为数值文本
+            text: ['高','低'],           
             calculable: true
         },
         toolbox: {
@@ -394,6 +405,11 @@ function showCandlestick(){
     var myChart = echarts.init(document.getElementById('main'));
   
     option = {
+        title: {
+            text: 'Candlestick Chart',
+            subtext: '',
+            left: 'center'
+        },
         xAxis: {
             data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
         },
@@ -428,10 +444,9 @@ function showRadar(){
   
     option = {
         title: {
-            text: '浏览器占比变化',
-            subtext: '纯属虚构',
-            top: 10,
-            left: 10
+            text: 'Radar Chart',
+            subtext: 'Number of User per Web Browser',
+            left: 'center'
         },
         tooltip: {
             trigger: 'item',
@@ -467,7 +482,7 @@ function showRadar(){
             var series = [];
             for (var i = 1; i <= 28; i++) {
                 series.push({
-                    name:'浏览器（数据纯属虚构）',
+                    name:'Browser（Data is not real）',
                     type: 'radar',
                     symbol: 'none',
                     lineStyle: {
@@ -526,6 +541,11 @@ data = data.map(function (item) {
 });
 
 option = {
+    title: {
+            text: 'Heatmap Chart',
+            subtext: '',
+            left: 'center'
+        },
     tooltip: {
         position: 'top'
     },
@@ -946,6 +966,11 @@ function showSunburst(){
     }];
     
     option = {
+        title: {
+            text: 'Sunbrust Chart',
+            subtext: 'Family Tree',
+            left: 'center'
+        },
         series: {
             type: 'sunburst',
             // highlightPolicy: 'ancestor',
@@ -972,6 +997,11 @@ function showSunburst(){
 function showParallel(){
     var myChart = echarts.init(document.getElementById('main'));
     option = {
+        title: {
+            text: 'Parallel Chart',
+            subtext: '',
+            left: 'center'
+        },
         parallelAxis: [
             {dim: 0, name: 'Price'},
             {dim: 1, name: 'Net Weight'},
@@ -1844,6 +1874,11 @@ var days = ['Saturday', 'Friday', 'Thursday',
 
 var data = [[0,0,5],[0,1,1],[0,2,0],[0,3,0],[0,4,0],[0,5,0],[0,6,0],[0,7,0],[0,8,0],[0,9,0],[0,10,0],[0,11,2],[0,12,4],[0,13,1],[0,14,1],[0,15,3],[0,16,4],[0,17,6],[0,18,4],[0,19,4],[0,20,3],[0,21,3],[0,22,2],[0,23,5],[1,0,7],[1,1,0],[1,2,0],[1,3,0],[1,4,0],[1,5,0],[1,6,0],[1,7,0],[1,8,0],[1,9,0],[1,10,5],[1,11,2],[1,12,2],[1,13,6],[1,14,9],[1,15,11],[1,16,6],[1,17,7],[1,18,8],[1,19,12],[1,20,5],[1,21,5],[1,22,7],[1,23,2],[2,0,1],[2,1,1],[2,2,0],[2,3,0],[2,4,0],[2,5,0],[2,6,0],[2,7,0],[2,8,0],[2,9,0],[2,10,3],[2,11,2],[2,12,1],[2,13,9],[2,14,8],[2,15,10],[2,16,6],[2,17,5],[2,18,5],[2,19,5],[2,20,7],[2,21,4],[2,22,2],[2,23,4],[3,0,7],[3,1,3],[3,2,0],[3,3,0],[3,4,0],[3,5,0],[3,6,0],[3,7,0],[3,8,1],[3,9,0],[3,10,5],[3,11,4],[3,12,7],[3,13,14],[3,14,13],[3,15,12],[3,16,9],[3,17,5],[3,18,5],[3,19,10],[3,20,6],[3,21,4],[3,22,4],[3,23,1],[4,0,1],[4,1,3],[4,2,0],[4,3,0],[4,4,0],[4,5,1],[4,6,0],[4,7,0],[4,8,0],[4,9,2],[4,10,4],[4,11,4],[4,12,2],[4,13,4],[4,14,4],[4,15,14],[4,16,12],[4,17,1],[4,18,8],[4,19,5],[4,20,3],[4,21,7],[4,22,3],[4,23,0],[5,0,2],[5,1,1],[5,2,0],[5,3,3],[5,4,0],[5,5,0],[5,6,0],[5,7,0],[5,8,2],[5,9,0],[5,10,4],[5,11,1],[5,12,5],[5,13,10],[5,14,5],[5,15,7],[5,16,11],[5,17,6],[5,18,0],[5,19,5],[5,20,3],[5,21,4],[5,22,2],[5,23,0],[6,0,1],[6,1,0],[6,2,0],[6,3,0],[6,4,0],[6,5,0],[6,6,0],[6,7,0],[6,8,0],[6,9,0],[6,10,1],[6,11,0],[6,12,2],[6,13,1],[6,14,3],[6,15,4],[6,16,0],[6,17,0],[6,18,0],[6,19,0],[6,20,1],[6,21,2],[6,22,2],[6,23,6]];
 option = {
+    title: {
+            text: '3D Bar Chart',
+            subtext: '',
+            left: 'center'
+        },
     tooltip: {},
     visualMap: {
         max: 20,
