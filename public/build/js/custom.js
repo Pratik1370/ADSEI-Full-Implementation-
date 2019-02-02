d8deb49f8661bc8075e3609ece289d245d098482
d-seerapu <vseerapu@deakin.edu.au>
2019-02-02 16:45:03 +1100

    dynamic servername test

--- public/build/js/custom.js
+++ public/build/js/custom.js
@@ -1007,7 +1007,7 @@ $(document).ready(function () {
 
 		}
 	};
-	xhttp.open("GET", "http://localhost:5000/users", true);
+	xhttp.open("GET", window.location.host+"/users", true);
 	xhttp.setRequestHeader("Content-type", "application/json");
 	xhttp.send();
 
